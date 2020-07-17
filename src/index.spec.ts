85be5e45609c9a3253dc79c17f83696b9ea5d075
Kevin Wang <kevmo314@gmail.com>
2020-07-17 02:14:56 -0400

    Update tsconfig.

--- src/index.spec.ts
+++
@@ -1,7 +0,0 @@
-import * as protooClient from "./index";
-import * as protooClientPkg from "../package.json";
-
-test("protooClient.version exposes the protoo-client package version", () => {
-  expect(typeof protooClient.version).toBe("string");
-  expect(protooClient.version).toBe(protooClientPkg.version);
-}, 500);
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-export { version } from "../package.json";
 export { default as Peer } from "./Peer";
 export { webSocketTransport } from "./transports/WebSocketTransport";
 export { RequestError, CodedError } from "./Message";
